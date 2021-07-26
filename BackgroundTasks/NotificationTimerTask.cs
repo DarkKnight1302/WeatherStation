411bc4d2f9eb490cb689bbc1faf899a9ae54901e
ROBIN PARASHAR <robin.parashar@outlook.com>
2021-07-26 23:25:13 +0530

    Create background project

---
+++ BackgroundTasks/NotificationTimerTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Windows.ApplicationModel.Background;
+
+namespace BackgroundTasks
+{
+    public sealed class NotificationTimerTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
