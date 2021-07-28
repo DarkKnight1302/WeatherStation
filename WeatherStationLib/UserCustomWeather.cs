e8756ac2acac953848a1b4c5a219d93d21bcb789
ROBIN PARASHAR <robin.parashar@outlook.com>
2021-07-28 13:34:27 +0530

    User custom weather class added.

---
+++ WeatherStationLib/UserCustomWeather.cs
@@ -0,0 +1,49 @@
+﻿
+namespace WeatherStationLib
+{
+    public sealed class UserCustomWeather
+    {
+        /// <summary>
+        /// Compare temperature only when this is true.
+        /// </summary>
+        public bool compareTemperature { get; set; }
+
+        public int temperatureMin { get; set; }
+
+        public int temperatureMax { get; set; }
+
+        /// <summary>
+        /// Compare humidity when this is true.
+        /// </summary>
+        public bool compareHumidity { get; set; }
+
+        public int humidityMin { get; set; }
+
+        public int humidityMax { get; set; }
+
+        /// <summary>
+        /// Compare weather condition only when this is true.
+        /// </summary>
+        public bool compareWeatherCondition { get; set; }
+
+        public WeatherCondition weatherCondition { get; set; }
+
+        /// <summary>
+        /// Compare wind speed only when this is true.
+        /// </summary>
+        public bool compareWindSpeed { get; set; }
+
+        public int windspeedMin { get; set; }
+
+        public int windspeedMax { get; set; }
+
+        /// <summary>
+        /// Compare cloudiness only when this is true.
+        /// </summary>
+        public bool compareCloudiness { get; set; }
+
+        public int cloudinessMin { get; set; }
+
+        public int cloudinessMax { get; set; }
+    }
+}
---
+++ WeatherStationLib/WeatherCondition.cs
@@ -0,0 +1,22 @@
+﻿
+namespace WeatherStationLib
+{
+    public enum WeatherCondition
+    {
+        Clear = 0,
+        Clouds = 1,
+        Rain = 2,
+        Tornado = 3,
+        Squall = 4,
+        Ash = 5,
+        Dust = 6,
+        Sand = 7,
+        Fog = 8,
+        Haze = 9,
+        Smoke = 10,
+        Mist = 11,
+        Snow = 12,
+        Drizzle = 13,
+        Thunderstorm = 14,
+    }
+}
